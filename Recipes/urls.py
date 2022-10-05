8642a92d0351f7357b1d8fa4099a65a2c5caa980
Vitor Mateus Hirt <zorinos.masterzorin@outlook.com>
2022-10-05 20:59:27 -0300

    if for

--- Recipes/urls.py
+++ Recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views  # abreviação de importação 
 
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.Home),
-    path('recipes/<int:id>/', views.Recipe),  # tornar as urls mais seguras 
+    path('', views.Home, name="home"),  # Forma antiga recipes-home tudo na mesma linha
+    path('recipes/<int:id>/', views.Recipe, name="recipe"),  # tornar as urls mais seguras 
 ]
